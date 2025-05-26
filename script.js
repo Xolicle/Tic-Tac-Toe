4e2906c431b928599a5762cbb70783b874adad4a
Xolicle <xoliegatsheni@gmail.com>
2025-05-26 23:09:33 +0200

    Adding players functionality

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const domManipulator = (function () {
     playerSection: document.querySelector(".playersSection"),
     gameIcon: document.querySelector(".gameIcon"),
     gamePlayers: document.querySelector(".gamePlayers"),
+    playerOneScore: document.querySelector("#playerOneScore"),
+    playerTwoScore: document.querySelector("#playerTwoScore"),
+    player1Name: document.querySelector("#player1Name"),
+    player2Name: document.querySelector("#player2Name"),
     turn: document.querySelector(".turn"),
     winner: document.querySelector(".winner"),
     getCells: function () {
@@ -70,6 +74,7 @@ const displayController = (function () {
   function Player(name, marker, type = "human") {
     return { name, marker, type };
   }
+  // console.log("display");
   domManipulator.gameboard.style.display = "none";
   domManipulator.solo.addEventListener("click", () => {
     // domManipulator.container.style.display = "none";
@@ -77,15 +82,65 @@ const displayController = (function () {
     domManipulator.gameIcon.style.display = "none";
     domManipulator.gamePlayers.style.display = "flex";
     domManipulator.gameboard.style.display = "grid";
+
     Gameboard.load();
-    // console.log("starting");
+    console.log("inside solo button");
+    humanPlayer(player1.marker);
   });
+  const player1 = Player("Human", "X");
+  const player2 = Player("Bot", "O");
+  let currentPlayer = player1;
+
   domManipulator.group.addEventListener("click", () => {
     domManipulator.playerSection.style.display = "none";
     domManipulator.gameIcon.style.display = "none";
     // domManipulator.gamePlayers.style.display = "flex";
     domManipulator.gameboard.style.display = "grid";
     Gameboard.load();
+    console.log("inside group button");
   });
+  function humanPlayer(mark) {
+    // console.log("checking");
+    domManipulator.getCells().forEach((cell) => {
+      cell.addEventListener("click", (e) => {
+        if (e.currentTarget.textContent == "") {
+          const index = Array.from(e.currentTarget.parentNode.children).indexOf(
+            e.currentTarget
+          );
+          // console.log("X");
+          Gameboard.cellMarker(mark, index);
+          takeTurn();
+        }
+      });
+    });
+  }
+  function switchPlayers() {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+  }
+  function takeTurn() {
+    let player = "";
+    if (currentPlayer == player1) {
+      player = player1;
+    } else {
+      player = player2;
+    }
+    humanPlayer(player1.marker);
+    console.log("Turn");
+  }
 })();
 const game = {};
+
+// domManipulator.getCells().forEach((cell) => {
+//   cell.addEventListener("click", () => {
+//     const clickedCell = e.currentTarget;
+//     const markSpan = clickedCell.querySelector(".markType");
+//     if (markSpan && markSpan.textContent === "") {
+//       const currentPlayerMark = "X";
+//       markSpan.textContent = currentPlayerMark;
+
+//       markSpan.classList.add("placed-mark");
+//       markSpan.classList.add(currentPlayerMark.toLowerCase());
+//     }
+//     console.log("X");
+//   });
+// });
